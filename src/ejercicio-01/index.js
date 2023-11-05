8d709a1669c91258caf47de55df24fb113dac623
juandaes21 <juandaes01@hotmail.com>
2023-11-05 13:15:02 -0500

    Añadir ejercicio 1

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,16 @@
+let nombre= 'Toalo'
+let edad= 22
+let esEstudiante=true
+let mensaje= 'Hola! '+ nombre + ', tu edad es '+ edad
+let num1=21
+let num2 = 11
+let suma= num1+num2
+let resta= num1-num2
+let multiplicacion= num1*num2
+let division= num1/num2
+
+console.log(mensaje)
+console.log('Suma:', suma)
+console.log('Resta:', resta)
+console.log('Multiplicación:', multiplicacion)
+console.log('División:', division)
